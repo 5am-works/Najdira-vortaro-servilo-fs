dab3323f7cd19ed65a370bca3b0f8883fa4952d0
Sam Wang <samofcorinth@gmx.ca>
2019-11-03 22:57:45 -0800

    Aldonis mian unuan finpunkton!

---
+++ NajdiraVortaroServilo/Controllers/VortojController.fs
@@ -0,0 +1,47 @@
+﻿namespace NajdiraVortaroServilo.Controllers
+
+open Microsoft.Extensions.Logging
+open Microsoft.AspNetCore.Mvc
+open NajdiraVortaroServilo.Data
+
+type VortoInformo =
+   { Vorto : string
+     Ecoj: uint8
+     Signifo : string
+     Egalvortoj : string array
+     Radikoj : string list
+     Idoj : string array
+   }
+
+[<ApiController>]
+[<Route("[controller]")>]
+type VortojController (logger:ILogger<VortojController>) =
+   inherit ControllerBase()
+
+   let dictionary = GetDictionary |> Async.RunSynchronously
+
+   [<HttpGet("{vorto}")>]
+   member this.Informi (vorto:string) : ActionResult =
+      dictionary.Vortoj
+      |> Array.tryFindIndex (fun v -> v.Vorto = vorto)
+      |> Option.map (fun v ->
+         let trovitaVorto = dictionary.Vortoj.[v]
+         let signifo = dictionary.Signifoj.[trovitaVorto.Signifo]
+         let egalVortoj =
+            dictionary.Vortoj
+            |> Array.filter
+               (fun e -> e.Signifo = trovitaVorto.Signifo && e.Vorto <> trovitaVorto.Vorto)
+            |> Array.map (fun e -> e.Vorto)
+         let idoj =
+            dictionary.Vortoj
+            |> Array.filter (fun i -> i.Radikoj |> List.exists (fun r -> v = r))
+            |> Array.map (fun i -> i.Vorto)
+         let radikoj =
+            trovitaVorto.Radikoj |> List.map (fun r -> dictionary.Vortoj.[r].Vorto)
+         { Vorto = trovitaVorto.Vorto
+           Ecoj = signifo.Ecoj
+           Signifo = signifo.Signifo 
+           Egalvortoj = egalVortoj
+           Radikoj = radikoj 
+           Idoj = idoj } |> this.Ok :> ActionResult)
+      |> Option.defaultValue (this.NotFound() :> ActionResult)
---
+++ NajdiraVortaroServilo/Data/Vortaro.fs
@@ -0,0 +1,39 @@
+﻿module NajdiraVortaroServilo.Data
+
+open FSharp.Data
+
+type VortaroJSON = JsonProvider<"http://raw.githubusercontent.com/Najdira/Najdira-redaktilo/master/vortaro.json">
+
+type Vorttipo =
+   | Substantivo
+   | Verbo
+   | Helpvorto
+   | Nekonita
+
+type Vorto = { Vorto : string; Signifo: int; Radikoj: int list }
+type Signifo = { Signifo : string; Ecoj: uint8; Tipo: Vorttipo; Ekstera: bool }
+type Vortaro = { Signifoj: Signifo[]; Vortoj: Vorto[] }
+
+let GetDictionary =
+   async {
+      let! rezultoj = VortaroJSON.AsyncGetSample()
+      let signifoj =
+         rezultoj.Signifoj
+         |> Array.map (fun r ->
+            { Signifo.Signifo = r.Signifo
+              Ecoj = uint8(r.Ecoj)
+              Tipo =
+                 match r.Tipo with
+                 | "substantivo" -> Substantivo
+                 | "verbo" -> Verbo
+                 | "helpvorto" -> Helpvorto
+                 | _ -> Nekonita
+              Ekstera = r.Ekstera })
+      let vortoj =
+         rezultoj.Vortoj
+         |> Array.map (fun r ->
+            { Vorto.Vorto = r.Vorto
+              Signifo = r.Signifo
+              Radikoj = r.Radikoj |> List.ofArray })
+      return { Vortaro.Signifoj = signifoj; Vortoj = vortoj }
+   }
